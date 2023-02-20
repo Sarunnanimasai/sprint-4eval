b5e2703250fc29f3607c16aff53a06628e07dac5
Sama Sarun Reddy <sarunnanimasaischool@gmail.com>
2023-02-20 17:26:57 +0530

    Update middleware.js

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -3,11 +3,11 @@ const jwt = require("jsonwebtoken");
 const Auth = (req, res, next) => {
   const token = req.headers.authorization;
   jwt.verify(token, "mytoken", (err, decoded) => {
-    if (err) {
-      res.send({ msg: "Please Login First" });
-    } else {
+    if (decoded) {
       req.body.userId = decoded.userId;
       next();
+    } else {
+      res.send({ msg: "Please Login First" });
     }
   });
 };
